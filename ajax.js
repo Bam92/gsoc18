4d3db0f8a167707194912826a7c84da2249b40ff
Abel <abelmbula@protonmail.com>
2018-02-19 23:09:53 +0200

    Links added
    each article/page point to its link on wikipedia

--- ajax.js
+++ ajax.js
@@ -1,6 +1,3 @@
-// Exécute un appel AJAX GET
-
-// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
 
 function ajaxGet(url, callback) {
 
@@ -9,25 +6,17 @@ function ajaxGet(url, callback) {
     req.open("GET", url);
 
     req.addEventListener("load", function () {
-
         if (req.status >= 200 && req.status < 400) {
-
-            // Appelle la fonction callback en lui passant la réponse de la requête
-
             callback(req.responseText);
 
         } else {
-
             console.error(req.status + " " + req.statusText + " " + url);
-
         }
 
     });
 
     req.addEventListener("error", function () {
-
-        console.error("Network erro " + url);
-
+        console.error("Network error " + url);
     });
 
     req.send(null);
--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 ajaxGet("http://localhost/gsoc18/petscan.json", function (reponse) {
 
-    // Transform the response into JavaScript objects
-
+    // Parse Data into objects.
     var queries = JSON.parse(reponse);
 
     // Display titles of pages
-
-    queries.forEach(function (query) {
-        var listElt = document.createElement("li"); // creating an li  tag
-        listElt.textContent = query.title; // here we define the content of the li tag
+    queries.forEach(function (query) {    
+        // link to the given article. use frwiki for this example        
+        var link = document.createElement("a");
+        link.textContent = query.title;
+        link.href = "https://fr.wikipedia.org/w/" + query.title;
         
-      document.getElementById("cities").appendChild(listElt);
+        var listElt = document.createElement("li"); // creating an li  tag        
+        //listElt.textContent = link; // here we define the content of the li tag
+        listElt.appendChild(link);
+        
+        
+        document.getElementById("cities").appendChild(listElt);
 
         console.log(query.title);
 
