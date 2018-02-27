73fbcbbc083e9960c1f3cf049bfac11e6b0bb6df
Abel <abelmbula@protonmail.com>
2018-02-27 22:28:30 +0200

    PEP 8 for imports
    Follow PEP 8 when importing modules

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 #! /usr/bin/python3.4
-
 # -*-coding:utf-8 -*
 
-import os, urllib.request, json
+"""This script download PetScan content in json format & write it on a file"""
+
+# import (PEP 8 convention)
+import os
+import urllib.request
+import json
 
 with urllib.request.urlopen("https://petscan.wmflabs.org/?format=json&psid=3316252") as url:
-    #os.chdir("/home/bam92/public_html/gsoc18")
+    
     data = json.loads(url.read().decode())
     with open(os.path.expanduser("~/public_html/gsoc18/test.json")) as f:
         print(data, file=f)
